534dbd11f0d372a03bce1e50421a469a100d07ea
Kristy Prieto <kprieto14@gmail.com>
2023-03-23 21:20:39 -0400

    Just finished adventure and epic mode! Might need more practice with joining but very interesting stuff.

--- queries.sql
+++ queries.sql
@@ -1,5 +1,5 @@
 CREATE TABLE "Employees"(
-                 "FullName" TEXT NOT null,
+                 "FullName" TEXT NOT NULL,
                  "Salary" INT,
                  "JobPosition" TEXT,
                  "PhoneExtension" varchar(3),
@@ -59,3 +59,31 @@ DELETE FROM "Employees" WHERE "FullName" = 'Lazy Larry';
 
 
 ALTER TABLE "Employees" ADD COLUMN "ParkingSpot" VARCHAR(10);
+
+
+ALTER TABLE "Employees" ADD COLUMN "Id" SERIAL PRIMARY KEY;
+
+
+CREATE TABLE "Departments"(
+  "Name" TEXT,
+  "BuildingNumber" INT,
+  "DepartmentId" SERIAL PRIMARY KEY
+);
+
+INSERT INTO "Departments" ("Name", "BuildingNumber")
+VALUES ('Main Building', 680);
+
+INSERT INTO "Departments" ("Name", "BuildingNumber")
+VALUES ('Work From Home', 1360);
+
+
+ALTER TABLE "Employees" ADD COLUMN "DepartmentId" INTEGER NULL REFERENCES "Departments" ("BuildingId");
+
+
+UPDATE "Employees" SET "DepartmentId" = 2 WHERE "Id" IN (1, 2, 4, 6, 12);
+UPDATE "Employees" SET "DepartmentId" = 1 WHERE "Id" IN (3, 5, 8, 9, 10, 11);
+
+
+SELECT * 
+FROM "Employees"
+JOIN "Departments" ON "Employees"."DepartmentId" = "Departments"."BuildingId";
